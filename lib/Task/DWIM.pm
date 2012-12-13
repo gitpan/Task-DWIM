cd0694cfcbe3f18f0a4ab3016ce28dd0638bd3fa
גאבור סבו - Gábor Szabó <gabor@pti.co.il>
2012-12-13 15:45:37 +0000

    Import of SZABGAB/Task-DWIM-0.01 from CPAN.
    gitpan-cpan-distribution: Task-DWIM
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SZABGAB/Task-DWIM-0.01.tar.gz
    gitpan-cpan-author:       SZABGAB
    gitpan-cpan-maturity:     released

---
+++ lib/Task/DWIM.pm
@@ -0,0 +1,42 @@
+package Task::Litmus;
+
+use 5.008;
+use strict;
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Task::DWIM - A Task module for DWIM Perl L<http://dwimperl.com/>
+
+=head1 DESCRIPTION
+
+Just a list of modules to be installed as part of the DWIM Perl distribution
+
+=head1 AUTHOR
+
+Gabor Szabo E<lt>szabgab@cpan.orgE<gt>
+
+If you are interested, contact me to take over the maintenance.
+
+=head1 SEE ALSO
+
+L<Task>, L<https://litmus.mozilla.org/>
+
+=head1 COPYRIGHT
+
+Copyright 2012 Gabor Szabo.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
+
